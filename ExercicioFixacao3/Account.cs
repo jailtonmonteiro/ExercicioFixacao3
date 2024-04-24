b83ab43af323b7d1a3523e2238218f8310a6114e
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-24 14:03:03 -0300

    Finalização de exercicio.

---
+++ ExercicioFixacao3/Account.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao3
+{
+    internal class Account
+    {
+        string _name;
+        int Number { get; set; }
+        double Balance { get; set; }
+        
+        public Account(string name, int number) 
+        {
+            this._name = name;
+            this.Number = number;
+        }
+
+        public void Deposit()
+        {
+            Console.Write("\nVALOR DEPÓSITO: ");
+            double value = double.Parse(Console.ReadLine());
+            Balance = Balance + value;
+            printData();
+        }
+        public void Withdraw()
+        {
+            Console.Write("\nVALOR SAQUE: ");
+            double value = double.Parse(Console.ReadLine());
+            Balance = Balance - 5 - value;
+            printData();
+        }
+        public void printData()
+        {
+            Console.WriteLine($"\nCONTA: {Number}\nTitular: {Nome}\nSaldo: {Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+
+        public string Nome
+        {
+            get { return this._name; }
+            set
+            {
+                if(value != null)
+                {
+                    this._name = value;
+                }
+            }
+        }
+    }
+}
--- ExercicioFixacao3/Program.cs
+++ ExercicioFixacao3/Program.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using System;
+
+namespace ExercicioFixacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, opt;
+            string name;
+            double value;
+
+            Console.Write("Numero da conta: ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome do titular: ");
+            name = Console.ReadLine();
+
+            Console.Write("Deposito inicial (s/n): ");
+            string op = Console.ReadLine();
+
+            Account account = new Account(name, number);
+            
+            if(op == "s")
+            {
+                account.Deposit();
+                account.printData();
+            }
+            else if(op == "n")
+            {
+                account.printData();
+            }
+            else
+            {
+                Console.WriteLine("Opção incorreta digitada!");
+            }
+            do
+            {
+                Console.Write("\n1 - DEPOSITAR\n2 - SACAR\n3 - SAIR\n\nSelecione uma opção: ");
+                opt = int.Parse(Console.ReadLine());
+
+                switch(opt)
+                {
+                    case 1:
+                        account.Deposit();
+                        break;
+                    case 2:
+                        account.Withdraw();
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("OPÇÃO SELECIONADA INVÁLIDA");
+                        break;
+                }
+            }while(opt != 3);
+
+        }
+    }
+}
